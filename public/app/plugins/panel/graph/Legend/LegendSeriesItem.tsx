8f1f06da0b9b87533f90867114b2fc36f990c4de
Mike-Farnhill <michael.farnhill@britishsteel.co.uk>
2020-05-20 15:11:02 +0100

    Remove word 'current' for last value in legend

--- public/app/plugins/panel/graph/Legend/LegendSeriesItem.tsx
+++ public/app/plugins/panel/graph/Legend/LegendSeriesItem.tsx
@@ -4,7 +4,7 @@ import { TimeSeries } from 'app/core/core';
 import { Icon, SeriesColorPicker } from '@grafana/ui';
 import { selectors } from '@grafana/e2e-selectors';
 
-export const LEGEND_STATS = ['min', 'max', 'avg', 'current', 'total'];
+export const LEGEND_STATS = ['min', 'max', 'avg', 'total', 'current'];
 
 export interface LegendLabelProps {
   series: TimeSeries;
@@ -200,7 +200,10 @@ interface LegendValueProps {
 
 function LegendValue(props: LegendValueProps) {
   const value = props.value;
-  const valueName = props.valueName;
+  let valueName = props.valueName;
+  if (valueName === 'current') {
+    valueName = '';
+  }
   if (props.asTable) {
     return <td className={`graph-legend-value ${valueName}`}>{value}</td>;
   }
